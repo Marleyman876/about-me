fc63fdb858ea57c87da8d3a4b7de92329ca96136
Marleyman876 <grant_garfield@yahoo.com>
2021-01-13 20:04:18 -0800

    fixed linter problem

--- js/app.js
+++ js/app.js
@@ -11,49 +11,49 @@ var answerOne = prompt('Am I from Jamaica?').toLocaleLowerCase();
 answerOne = answerOne.toLocaleLowerCase();
 
 if (answerOne === 'yes' || answerOne === 'y') {
-    // console.log('that is correct');
-    alert('that is correct');
+  // console.log('that is correct');
+  alert('that is correct');
 } else {
-    alert('I have great vibe of course I\'m Jamaica.');
+  alert('I have great vibe of course I\'m Jamaica.');
 }
 
 var answerTwo = prompt('Do you like coffee?');
 answerTwo = answerTwo.toLocaleLowerCase();
 
 if (answerTwo === 'yes' || answerTwo === 'y') {
-    // console.log('That is correct I do like coffee');
-    alert('That is correct I do like coffee');
+  // console.log('That is correct I do like coffee');
+  alert('That is correct I do like coffee');
 } else {
-    alert('You\'ve commited a great SIN!');
+  alert('You\'ve commited a great SIN!');
 }
 
 var answerThree = prompt('Did I go to college?');
 answerThree = answerThree.toLocaleLowerCase();
 
 if (answerThree === 'yes' || answerThree === 'y') {
-    // console.log('that is correct');
-    alert('That is correct I went to the University of Marland');
+  // console.log('that is correct');
+  alert('That is correct I went to the University of Marland');
 } else {
-    alert('You take that back I\'m SMART!');
+  alert('You take that back I\'m SMART!');
 }
 
 var answerFour = prompt('Is swimming a hobby of mine?');
 answerFour = answerFour.toLocaleLowerCase();
 
 if (answerFour === 'yes' || answerFour === 'y') {
-    // console.log ('That is correct I grew up on an island not only do i swim I\'m also Scuba certified, #aquaman');
-    alert('That is correct I grew up on an island not only do i swim I\'m also Scuba certified, #aquaman');
+  // console.log ('That is correct I grew up on an island not only do i swim I\'m also Scuba certified, #aquaman');
+  alert('That is correct I grew up on an island not only do i swim I\'m also Scuba certified, #aquaman');
 } else {
-    alert('seriously I grew up on an island I was born swimming!');
+  alert('seriously I grew up on an island I was born swimming!');
 }
 
 var answerFive = prompt('Was I in the Amry?');
 answerFive = answerFive.toLocaleLowerCase();
 
 if (answerFive === 'yes' || answerFive === 'y') {
-    alert('That is correct, I served 8 looong years!');
+  alert('That is correct, I served 8 looong years!');
 } else {
-    alert('you better fix yourself I\'m an American Hero!');
+  alert('you better fix yourself I\'m an American Hero!');
 }
 
 alert('Thank you ' + (userName) + ' for visiting my page and learning about me, have a great day!');

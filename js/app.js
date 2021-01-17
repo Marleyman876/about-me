527b10242cf5b4386fa47f9dabecd6f6f143ce69
Marleyman876 <grant_garfield@yahoo.com>
2021-01-16 17:06:35 -0800

    Added question prompt #7

--- js/app.js
+++ js/app.js
@@ -1,70 +1,75 @@
 'use strict';
 
-// // get username and offer greeting
-// var userName = prompt('Hello, what\'s your name?');
-// alert('Hello ' + (userName) + ' welcome to my about me page, lets see if you read my bio.');
+var score = 0;
+
+// get username and offer greeting
+var userName = prompt('Hello, what\'s your name?');
+alert('Hello ' + (userName) + ' welcome to my about me page, lets see if you read my bio.');
 
 
 // //wrtire 5 questions: they must accept yes or no in any case
 
-// var answerOne = prompt('Am I from Jamaica?');
-// answerOne = answerOne.toLocaleLowerCase();
+var answerOne = prompt('Am I from Jamaica?');
+answerOne = answerOne.toLocaleLowerCase();
 
-// if (answerOne === 'yes' || answerOne === 'y') {
-//   // console.log('that is correct');
-//   alert('that is correct');
-// } else {
-//   alert('I have great vibe of course I\'m Jamaica.');
-// }
+if (answerOne === 'yes' || answerOne === 'y') {
+  // console.log('that is correct');
+  alert('that is correct');
+  score++;
+} else {
+  alert('I have great vibe of course I\'m Jamaica.');
+}
 
-// var answerTwo = prompt('Do you like coffee?');
-// answerTwo = answerTwo.toLocaleLowerCase();
+var answerTwo = prompt('Do you like coffee?');
+answerTwo = answerTwo.toLocaleLowerCase();
 
-// if (answerTwo === 'yes' || answerTwo === 'y') {
-//   // console.log('That is correct I do like coffee');
-//   alert('That is correct I do like coffee');
-// } else {
-//   alert('You\'ve commited a great SIN!');
-// }
+if (answerTwo === 'yes' || answerTwo === 'y') {
+  // console.log('That is correct I do like coffee');
+  alert('That is correct I do like coffee');
+  score++;
+} else {
+  alert('You\'ve commited a great SIN!');
+}
 
-// var answerThree = prompt('Did I go to college?');
-// answerThree = answerThree.toLocaleLowerCase();
+var answerThree = prompt('Did I go to college?');
+answerThree = answerThree.toLocaleLowerCase();
 
-// if (answerThree === 'yes' || answerThree === 'y') {
-//   // console.log('That is correct, I went to the university of Maryland');
-//   alert('That is correct I went to the University of Marland');
-// } else {
-//   alert('You take that back I\'m SMART!');
-// }
+if (answerThree === 'yes' || answerThree === 'y') {
+  // console.log('That is correct, I went to the university of Maryland');
+  alert('That is correct I went to the University of Marland');
+  score++;
+} else {
+  alert('You take that back I\'m SMART!');
+}
 
-// var answerFour = prompt('Is swimming a hobby of mine?');
-// answerFour = answerFour.toLocaleLowerCase();
+var answerFour = prompt('Is swimming a hobby of mine?');
+answerFour = answerFour.toLocaleLowerCase();
+if (answerFour === 'yes' || answerFour === 'y') {
+  // console.log ('That is correct I grew up on an island not only do I swim I\'m also Scuba certified, #aquaman');
+  alert('That is correct I grew up on an island not only do I swim I\'m also Scuba certified, #aquaman');
+  score++;
+} else {
+  alert('Seriously I grew up on an island I was born swimming!');
+}
 
-// if (answerFour === 'yes' || answerFour === 'y') {
-//   // console.log ('That is correct I grew up on an island not only do I swim I\'m also Scuba certified, #aquaman');
-//   alert('That is correct I grew up on an island not only do I swim I\'m also Scuba certified, #aquaman');
-// } else {
-//   alert('Seriously I grew up on an island I was born swimming!');
-// }
+var answerFive = prompt('Was I in the Amry?');
+answerFive = answerFive.toLocaleLowerCase();
 
-// var answerFive = prompt('Was I in the Amry?');
-// answerFive = answerFive.toLocaleLowerCase();
+if (answerFive === 'yes' || answerFive === 'y') {
+  // console.log ('That is correct, I served 8 looong years!');
+  alert('That is correct, I served 8 looong years!');
+  score++;
+} else {
+  alert('you better fix yourself I\'m an American Hero!');
+}
 
-// if (answerFive === 'yes' || answerFive === 'y') {
-//   // console.log ('That is correct, I served 8 looong years!');
-//   alert('That is correct, I served 8 looong years!');
-// } else {
-//   alert('you better fix yourself I\'m an American Hero!');
-// }
 
-// alert(`Goodbye, ${userName} thank you for visitng my page and learning about me, I hope you have a great day.`);
+
 
 // alert('Thank you ' + (userName) + ' for visiting my page and learning about me, have a great day!');
 
 
-
-
-// Lab03
+// question 6
 
 
 var maxCount = 4;
@@ -73,6 +78,7 @@ for (var i = 0; i < maxCount; i++) {
   var questionSixx = parseInt(prompt('what is the lonliest number?'));
   if (questionSixx === number) {
     alert('your are correct');
+    score ++;
     break;
   } else if (questionSixx > number) {
     alert('too high, guess again');
@@ -80,10 +86,38 @@ for (var i = 0; i < maxCount; i++) {
     alert('too low try again');
   }
   if (i === maxCount - 1) {
-    alert('the correct answer is 1');
+    alert(`the correct answer is, ${number}`);
 
   }
 
 }
 
-// myArray[`black`, `red`, `blue`];
+// question 7
+
+var favoriteEvent = ['dancehall', 'mass', 'concert', 'play', 'going to the movies'];
+
+var answerCorrectly = false;
+
+var count = 6;
+
+for ( i = 0; i < count; i++) {
+  var party = prompt('what is my favorite party to attend');
+
+  for (var j = 0; j < favoriteEvent.length; j++) {
+    if (party === favoriteEvent[j]) {
+      alert(`you are correct, ${favoriteEvent[j]} is my favourite event`);
+      answerCorrectly = true;
+    }
+  }
+  if (answerCorrectly) {
+    break;
+  }
+
+}
+if (i === count || answerCorrectly) {
+  alert(`My favortie events are ${favoriteEvent}`);
+
+}
+
+
+alert(`Goodbye, ${userName} thank you for visitng my page and learning about me, I hope you have a great day. Your score for today is ${score}`);
